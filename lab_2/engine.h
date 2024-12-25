489780470978e227b117d7e8197c1db74196630b
oksana valenko <oksanavalenko@yandex.ru>
2024-12-25 12:55:34 +0700

    fix oop methods

--- lab_2/engine.h
+++ lab_2/engine.h
@@ -8,8 +8,5 @@
 class Engine{
 public:
     virtual void BlackJack(std::vector<std::unique_ptr<Strategy>>& strategy_)=0;
-    //virtual void Game(const std::unique_ptr<Player>& player_1,const std::unique_ptr<Player>& player_2,const std::string& CurDeck,const int& deck_data,const std::string& CurInter)=0;
-    //virtual const std::unique_ptr<Player>& ChooseWinner(const std::unique_ptr<Player>& pl_1,const std::unique_ptr<Player>& pl_2) =0;
-    //virtual void EndGame(const std::unique_ptr<Player>& pl_1,const std::unique_ptr<Player>& pl_2) =0;
-    //virtual ~Engine()=default;
+    virtual ~Engine()=default;
 };
